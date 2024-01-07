805ea7e9901feaf3e4de1727cf6be79437883399
Lucashcr <lucash.rocha@hotmail.com>
2024-01-06 21:40:25 -0300

    Fix menu layout

--- app.vue
+++ app.vue
@@ -26,62 +26,73 @@ useHead({
 <template>
   <v-app>
     <v-navigation-drawer app expand-on-hover rail permanent>
-      <v-list>
-        <v-list-item
-          v-for="item in $store.startItems"
-          :key="item.to"
-          :to="item.to"
-          router
-          exact
-          :prepend-icon="item.icon"
-          :title="item.title"
-        ></v-list-item>
-      </v-list>
-      <v-list>
-        <v-list-item
-          v-if="auth.isAuthenticated"
-          v-for="item in $store.hymnaryItems"
-          :key="item.to"
-          :to="item.to"
-          router
-          exact
-          :prepend-icon="item.icon"
-          :title="item.title"
-        ></v-list-item>
-      </v-list>
-      <v-list>
-        <v-list-item
-          v-if="auth.isAuthenticated"
-          v-for="item in $store.authItems"
-          :key="item.to"
-          :to="item.to"
-          router
-          exact
-          :prepend-icon="item.icon"
-          :title="item.title"
-        >
-        </v-list-item>
-        <v-list-item
-          v-if="!auth.isAuthenticated"
-          v-for="item in $store.notAuthItems"
-          :key="item.to"
-          :to="item.to"
-          router
-          exact
-          :prepend-icon="item.icon"
-          :title="item.title"
-        >
-        </v-list-item>
-        <v-list-item
-          v-for="item in $store.endItems"
-          :key="item.to"
-          :to="item.to"
-          router
-          exact
-          :prepend-icon="item.icon"
-          :title="item.title"
-        ></v-list-item>
-      </v-list>
+      <v-container class="fill-height pa-0 flex-column">
+        <v-col>
+          <v-row>
+            <v-list>
+              <v-list-item
+                v-for="item in $store.startItems"
+                :key="item.to"
+                :to="item.to"
+                router
+                exact
+                :prepend-icon="item.icon"
+                :title="item.title"
+              ></v-list-item>
+            </v-list>
+            <v-list>
+              <v-list-item
+                v-if="auth.isAuthenticated"
+                v-for="item in $store.hymnaryItems"
+                :key="item.to"
+                :to="item.to"
+                router
+                exact
+                :prepend-icon="item.icon"
+                :title="item.title"
+              ></v-list-item>
+            </v-list>
+          </v-row>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col class="flex-grow-0">
+          <v-row>
+            <v-list>
+              <v-list-item
+                v-if="auth.isAuthenticated"
+                v-for="item in $store.authItems"
+                :key="item.to"
+                :to="item.to"
+                router
+                exact
+                :prepend-icon="item.icon"
+                :title="item.title"
+              >
+              </v-list-item>
+              <v-list-item
+                v-if="!auth.isAuthenticated"
+                v-for="item in $store.notAuthItems"
+                :key="item.to"
+                :to="item.to"
+                router
+                exact
+                :prepend-icon="item.icon"
+                :title="item.title"
+              >
+              </v-list-item>
+              <v-list-item
+                v-for="item in $store.endItems"
+                :key="item.to"
+                :to="item.to"
+                router
+                exact
+                :prepend-icon="item.icon"
+                :title="item.title"
+              ></v-list-item>
+            </v-list>
+          </v-row>
+        </v-col>
+      </v-container>
     </v-navigation-drawer>
     <v-app-bar :elevation="2">
       <v-img
@@ -100,6 +111,10 @@ useHead({
 </template>
 
 <style scoped>
+.v-list {
+  width: 100%;
+}
+
 .v-list-item {
   padding-top: 20px;
   padding-bottom: 20px;
